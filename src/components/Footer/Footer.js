35fa303bc38a1b0d0181895d73672b69264e7c84
Nguyen-Kha <khnguyen26@gmail.com>
2022-07-03 23:02:15 -0400

    moved pages around

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Footer.css';
+
+function Footer() {
+    return(
+        <div className="footer">
+            <div className="footer_left">Made with Hate</div>
+            <div className="footer_middle">Some links</div>
+            <div className="footer_right">Copyright Kha Nguyen 2022</div>
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,22 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        <>
+            <h1>Home</h1>
+            <div className="opener">
+                <div className="opener_content">
+                    <h1>Hey, I'm Kha</h1>
+                    <h3>Data is knowledge and knowledge is power</h3>
+                    <p>Lorem Ipsum</p>
+                </div>
+                <div className="opener_picture"></div>
+                
+            </div>
+        </>
+        
+    );
+}
+
+export default Home;
